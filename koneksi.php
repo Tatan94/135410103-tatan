386c72840d6135a82ff492944f89e0085aeb6f02
Tatan94 <lalu.tatan94@gmail.com>
2016-06-23 14:39:04 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$host = mysql_connect("localhost","adminvx4iiBm","xvIY4YhTHDjY");
+ 
+// isikan dengan nama database yang akan di hubungkan
+$db = mysql_select_db("tatan-lalu");
+ 
+?>
